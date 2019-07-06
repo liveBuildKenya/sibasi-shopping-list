d1fc88c515c47fe752f7db0ca82c491dc622a8bd
liveBuildKenya <52596704+liveBuildKenya@users.noreply.github.com>
2019-07-06 12:51:02 +0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@
 export const environment = {
     production: false,
     firebase: {
-      apiKey: "AIzaSyD-afoc4k-jIlPjqwoqLOHreXMnpNQe5XY",
-      authDomain: "sibasi-9f914.firebaseapp.com",
-      databaseURL: "https://sibasi-9f914.firebaseio.com",
-      projectId: "sibasi-9f914",
-      storageBucket: "sibasi-9f914.appspot.com",
-      messagingSenderId: "579185107668",
-      appId: "1:579185107668:web:6dba7672b7a5b2cd"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     },
     registrationCode: ''
   };
